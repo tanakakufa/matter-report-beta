0974dfd6d84e1cc2b70e0e44003254ce1908eafa
Victo-Jorge <chitowamombevictojorge05@gmail.com>
2025-02-27 14:26:35 +0200

    Initial Commit

---
+++ Matter_Report/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Matter_Report
+//
+//  Created by Victo-Jorge on 27/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Matter_Report/Matter_ReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matter_ReportApp.swift
+//  Matter_Report
+//
+//  Created by Victo-Jorge on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Matter_ReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
