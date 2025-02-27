9c41020fde32d393a797cbdce04a8836afb57553
Victo-Jorge <chitowamombevictojorge05@gmail.com>
2025-02-27 18:34:55 +0200

    saving reports file

--- Matter_Report/Saving_Reports file.swift
+++ Matter_Report/Saving_Reports file.swift
@@ -6,13 +6,102 @@
 //
 
 import SwiftUI
+import Supabase
+import Foundation
 
-struct Saving_Reports_file: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// This if for saving reports in relation to a specific account.
+
+struct ProfileView: View {
+  @State var username = ""
+  @State var fullName = ""
+  @State var website = ""
+
+  @State var isLoading = false
+
+  var body: some View {
+    NavigationStack {
+      Form {
+        Section {
+          TextField("Username", text: $username)
+            .textContentType(.username)
+            .textInputAutocapitalization(.never)
+          TextField("Full name", text: $fullName)
+            .textContentType(.name)
+          TextField("Website", text: $website)
+            .textContentType(.URL)
+            .textInputAutocapitalization(.never)
+        }
+
+        Section {
+          Button("Update profile") {
+            updateProfileButtonTapped()
+          }
+          .bold()
+
+          if isLoading {
+            ProgressView()
+          }
+        }
+      }
+      .navigationTitle("Profile")
+      .toolbar(content: {
+        ToolbarItem(placement: .topBarLeading){
+          Button("Sign out", role: .destructive) {
+            Task {
+              try? await supabase.auth.signOut()
+            }
+          }
+        }
+      })
     }
-}
+    .task {
+      await getInitialProfile()
+    }
+  }
 
-#Preview {
-    Saving_Reports_file()
+  func getInitialProfile() async {
+    do {
+      let currentUser = try await supabase.auth.session.user
+
+      let profile: Profile =
+      try await supabase
+        .from("profiles")
+        .select()
+        .eq("id", value: currentUser.id)
+        .single()
+        .execute()
+        .value
+
+      self.username = profile.username ?? ""
+      self.fullName = profile.fullName ?? ""
+      self.website = profile.website ?? ""
+
+    } catch {
+      debugPrint(error)
+    }
+  }
+
+  func updateProfileButtonTapped() {
+    Task {
+      isLoading = true
+      defer { isLoading = false }
+      do {
+        let currentUser = try await supabase.auth.session.user
+
+        try await supabase
+          .from("profiles")
+          .update(
+            UpdateProfileParams(
+              username: username,
+              fullName: fullName,
+              website: website
+            )
+          )
+          .eq("id", value: currentUser.id)
+          .execute()
+      } catch {
+        debugPrint(error)
+      }
+    }
+  }
 }
