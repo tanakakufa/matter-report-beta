cf849638385cda0bc03948c2563f06d482e2deb4
Victo-Jorge <chitowamombevictojorge05@gmail.com>
2025-02-27 18:25:11 +0200

    Log in page

--- Matter_Report/Log_In Page.swift
+++ Matter_Report/Log_In Page.swift
@@ -7,12 +7,80 @@
 
 import SwiftUI
 
-struct Log_In_Page: View {
+struct Login: View {
+    @ObservedObject var viewModel: SignUpViewModel
+    @State private var presentAlert: Bool = false
+    @State private var errorMessage: String = ""
+    @State private var navigateToProfile: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+        
+            Text("Matter report")
+                .font(.custom("Georgia", size: 38.9))
+                .fontWeight(.semibold)
+            
+           
+            }
+        
+        
+            
+        VStack {
+            TextField("Enter your email address", text: $viewModel.email)
+                .padding()
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+                .background(Color.gray)
+                .cornerRadius(15)
+                .shadow(radius: 2)
+                .padding()
+
+            SecureField("Enter your password", text: $viewModel.password)
+                .padding()
+                .autocapitalization(.none)
+                .background(Color.gray)
+                .cornerRadius(15)
+                .shadow(radius: 2)
+                .padding()
+
+            Button("Login") {
+                Task {
+                    let success = await viewModel.signIn()
+                    if success {
+                        presentAlert = true
+                    } else {
+                        errorMessage = viewModel.errorMessage
+                    }
+                }
+            }
+            .padding()
+            .background(Color(.blue))
+            .cornerRadius(20)
+            .border(.white)
+            .disabled(viewModel.email.isEmpty || viewModel.password.isEmpty)
+
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding(.top, 10)
+            }
+
+            Spacer()
+        }
+    
+        .alert("Welcome", isPresented: $presentAlert) {
+            Button("OK", role: .cancel) {
+                navigateToProfile = true
+            }
+            
+        }
+        NavigationLink(destination: ProfileView(),isActive: $navigateToProfile){
+            EmptyView()
+        }
     }
 }
 
+
 #Preview {
-    Log_In_Page()
+    Login(viewModel: SignUpViewModel())
 }
