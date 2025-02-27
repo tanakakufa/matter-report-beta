035ffa87c7da7365327dfad75ccea8b876b6f16a
Victo-Jorge <chitowamombevictojorge05@gmail.com>
2025-02-27 18:24:25 +0200

    Authenticating with supabase.

--- Matter_Report/Authentication_View.swift
+++ Matter_Report/Authentication_View.swift
@@ -1,18 +1,71 @@
-//
-//  Authentication_View.swift
-//  Matter_Report
-//
-//  Created by Victo-Jorge on 27/02/2025.
-//
-
 import SwiftUI
+import Supabase
 
-struct Authentication_View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct AuthView: View {
+    
+  @State var email = ""
+    @State var password = ""
+  @State var isLoading = false
+
+  @State var result: Result<Void, Error>?
+
+  var body: some View {
+    Form {
+      Section {
+        TextField("Email", text: $email)
+          .textContentType(.emailAddress)
+          .textInputAutocapitalization(.never)
+          .autocorrectionDisabled()
+          TextField("password", text: $password)
+      }
+
+      Section {
+        Button("Sign up") {
+          signInButtonTapped()
+        }
+         
+
+        if isLoading {
+          ProgressView()
+        }
+      }
+
+      if let result {
+        Section {
+          switch result {
+          case .success:
+            Text("Check your inbox.")
+          case .failure(let error):
+            Text(error.localizedDescription).foregroundStyle(.red)
+          }
+        }
+      }
     }
-}
+    .onOpenURL(perform: { url in
+      Task {
+        do {
+          try await supabase.auth.session(from: url)
+        } catch {
+          self.result = .failure(error)
+        }
+      }
+    })
+  }
 
-#Preview {
-    Authentication_View()
+  func signInButtonTapped() {
+    Task {
+      isLoading = true
+      defer { isLoading = false }
+
+      do {
+        try await supabase.auth.signInWithOTP(
+            email: email,
+            redirectTo: URL(string: "io.supabase.user-management://login-callback")
+        )
+        result = .success(())
+      } catch {
+        result = .failure(error)
+      }
+    }
+  }
 }
