4a7e139c928435d88ad405b5eb74ba1609251ca1
Mildred <ngomamildred823@gmail.com>
2025-02-28 11:10:47 +0200

    Its for the notes saved

---
+++ Matter_Report/SaveReport.swift
@@ -0,0 +1,19 @@
+//
+//  SaveReport.swift
+//  Matter_Report
+//
+//  Created by Mildred Ngoma  on 2/28/25.
+//
+
+import SwiftUI
+
+struct SaveReport: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SaveReport()
+}
+
